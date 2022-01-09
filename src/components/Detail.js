c9dbe41147d90f505f3a044816c8bb8573b59f8b
Agustin <agustin.piperno@gmail.com>
2022-01-09 17:57:42 -0300

    ADD: grab movies from db

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -52,7 +52,7 @@ const Background = styled.div`
     right: 0;
     z-index: -1;
     opacity: 0.8;
-    
+
     img {
         width: 100%;
         height: 100%;
@@ -64,7 +64,9 @@ const ImageTitle = styled.div`
     min-height: 170px;
     width: 35vw;
     min-width: 200px;
-  
+    margin-top: 20px;
+    margin-left: -90px;
+
     img{
         width: 100%;
         height: 100%;
@@ -137,4 +139,5 @@ const Description = styled.div`
     font-size: 20px;
     margin-top: 16px;
     color: rgb(249, 249, 249);
+    max-width: 760px;
 `
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,35 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
+import {selectMovies} from '../features/movie/movieSlice'
+import {useSelector} from "react-redux"
 
 function Movies() {
+    const movies = useSelector(selectMovies);
+    console.log("this is movies", movies);
+
     return (
         <Container>
             <h4>Recommended for You</h4>
             <Content>
-                <Wrap>
-                    <img src="images/watchlist-icon.svg"></img>
-                </Wrap>
-                <Wrap>
-                    <img src="images/watchlist-icon.svg"></img>
-                </Wrap>
-                <Wrap>
-                    <img src="images/watchlist-icon.svg"></img>
-                </Wrap>
-                <Wrap>
-                    <img src="images/watchlist-icon.svg"></img>
-                </Wrap>
-                <Wrap>
-                    <img src="images/watchlist-icon.svg"></img>
-                </Wrap>
-                <Wrap>
-                    <img src="images/watchlist-icon.svg"></img>
-                </Wrap>
-                <Wrap>
-                    <img src="images/watchlist-icon.svg"></img>
-                </Wrap>
-                <Wrap>
-                    <img src="images/watchlist-icon.svg"></img>
-                </Wrap>
+                {movies &&
+                    movies.map((movie)=>(
+                        <Wrap key={movie.id}>
+                            <img src={movie.cardImg} alt="movie-pics"/>
+                        </Wrap>
+                    )
+                    ) 
+                }
             </Content>
         </Container>
     )
