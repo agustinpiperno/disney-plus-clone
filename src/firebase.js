d95737492f514116180de637a24c03393ecbd287
Agustin <agustin.piperno@gmail.com>
2022-01-09 21:15:54 -0300

    ADD: authDomain

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import firebase from "firebase";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD-jQZf1KCG3Wgr7_kf06cwfhm1bl_Kj8M",
-  authDomain: "disney-plus-clone-c3c6b.firebaseapp.com",
+  authDomain: "https://disneyplusap.netlify.app",
   projectId: "disney-plus-clone-c3c6b",
   storageBucket: "disney-plus-clone-c3c6b.appspot.com",
   messagingSenderId: "6672829623",
